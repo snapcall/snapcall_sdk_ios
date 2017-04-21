dfe5117561870a926d84121596138214983bf522
Noyelle <noyelle@iMac-de-Noyelle.local>
2017-04-21 11:10:54 +0200

    test

---
+++ snapcall_framework.framework/Headers/snapcall_framework.h
@@ -0,0 +1,19 @@
+//
+//  snapcall_framework.h
+//  snapcall_framework
+//
+//  Created by Noyelle on 20/04/2017.
+//  Copyright © 2017 Seampl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for snapcall_framework.
+FOUNDATION_EXPORT double snapcall_frameworkVersionNumber;
+
+//! Project version string for snapcall_framework.
+FOUNDATION_EXPORT const unsigned char snapcall_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <snapcall_framework/PublicHeader.h>
+
+
